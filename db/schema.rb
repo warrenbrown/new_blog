46fae463180b4aa708bb8eb7d069ccb8f5e2d687
warrenbrown <warren@katapultmedia.com>
2015-03-05 14:15:30 -0600

    added post, and comments models views, controllers and validation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305154500) do
+ActiveRecord::Schema.define(version: 20150305160205) do
+
+  create_table "comments", force: true do |t|
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "post_id"
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: true do |t|
     t.string   "title"
